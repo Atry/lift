2400b4a0692d3d1e4150166873571fcc75994bdd
bhuztez <bhuztez@gmail.com>
2016-08-26 13:28:48 +0800

    proper array indices generation

--- lift/codegen.py
+++ lift/codegen.py
@@ -18,6 +18,7 @@ OP = {
     'add': '+',
     'eq': '==',
     'and_': '&&',
+    'div': '/',
     'pdiv_r': '%',
     'zdiv_r': '%',
 }
@@ -35,73 +36,36 @@ def convert_expr(expr):
         raise NotImplementedError
 
 
-def to_basic_maps(map):
-    maps = []
-    map.foreach_basic_map(lambda x: maps.append(x))
-    return maps
-
-
-def transform_var(bmap, args):
-    bmap, = to_basic_maps(bmap.affine_hull())
-    n_out = bmap.dim(isl.dim_type.out)
-    n_in_ = bmap.dim(isl.dim_type.in_)
-
-    out_args = [None for i in xrange(n_out)]
-
-    for constraint in bmap.get_constraints():
-        if not constraint.is_equality():
-            continue
-        if not constraint.involves_dims(isl.dim_type.out, 0, n_out):
-            continue
-
-        index = ('int', -constraint.get_constant_val().to_python())
-
-        for i, v in enumerate(constraint.get_coefficient_val(isl.dim_type.in_, i) for i in xrange(n_in_)):
-            if v.is_zero():
-                continue
-
-            index1 = args[i]
-
-            if not v.is_negone():
-                index1 = ('call', '*', (('int', -v.to_python()), index1))
-
-            if index == ('int', 0):
-                index = index1
-            elif v.is_one():
-                index = ('call', '-', (index, index1[2][1]))
-            else:
-                index = ('call', '+', (index, index1))
-
-        (out_index, out_coeff), = (
-            (i, v.to_python())
-            for i, v in enumerate(constraint.get_coefficient_val(isl.dim_type.out, i) for i in xrange(n_out))
-            if not v.is_zero())
-
-        if out_coeff != 1:
-            index = ('call', '/', (index, ('int', out_coeff)))
-
-        out_args[out_index] = index
-
-    assert all(a is not None for a in out_args)
-    return ('element', bmap.get_tuple_name(isl.dim_type.out), tuple(out_args))
-
-
-def transform_expr(expr, args):
-    if expr[0] == 'var':
-        return transform_var(expr[1], args)
-    elif expr[0] == 'const':
-        return expr
-    elif expr[0] == 'call':
-        return ('call', expr[1], tuple(transform_expr(e, args) for e in expr[2]))
+def build_ast(stmts, node):
+    if node.get_type() == isl.ast_node_type.block:
+        return [build_ast(stmts, c) for c in node_list(node.block_get_children())]
+    elif node.get_type() == isl.ast_node_type.for_:
+        return ("for",
+            convert_expr(node.for_get_iterator()),
+            convert_expr(node.for_get_init()),
+            convert_expr(node.for_get_inc()),
+            convert_expr(node.for_get_cond()),
+            build_ast(stmts, node.for_get_body()))
+    elif node.get_type() == isl.ast_node_type.if_:
+        if node.if_has_else():
+            return (
+                "ifelse",
+                convert_expr(node.if_get_cond()),
+                build_ast(stmts, node.if_get_then()),
+                build_ast(stmts, node.if_get_else()))
+        else:
+            return (
+                "if",
+                convert_expr(node.if_get_cond()),
+                build_ast(stmts, node.if_get_then()))
+    elif node.get_type() == isl.ast_node_type.user:
+        name = node.user_get_expr().get_id().get_name()
+        return stmts[name]
     else:
         raise NotImplementedError
 
 
-def convert_stmt(ctx, expr):
-    assert isl.ast_op_type.find_value(expr.get_op_type()) == 'call'
-    name = expr.get_op_arg(0).get_id().get_name()
-    args = tuple(convert_expr(expr.get_op_arg(i)) for i in xrange(1, expr.get_op_n_arg()))
-
+def get_stmt(ctx, name):
     if name in ctx.def_stmts:
         stmt = ctx.def_stmts[name]
     elif name in ctx.init_stmts:
@@ -112,37 +76,63 @@ def convert_stmt(ctx, expr):
         stmt = ctx.fini_stmts[name]
     else:
         raise NotImplementedError
-
-    return ("assign", transform_var(stmt[0], args),
-            transform_expr(stmt[1], args), name)
+    return stmt
 
 
-def build_ast(ctx, node):
-    if node.get_type() == isl.ast_node_type.block:
-        return [build_ast(ctx, c) for c in node_list(node.block_get_children())]
-    elif node.get_type() == isl.ast_node_type.for_:
-        return ("for",
-            convert_expr(node.for_get_iterator()),
-            convert_expr(node.for_get_init()),
-            convert_expr(node.for_get_inc()),
-            convert_expr(node.for_get_cond()),
-            build_ast(ctx, node.for_get_body()))
-    elif node.get_type() == isl.ast_node_type.if_:
-        if node.if_has_else():
-            return (
-                "ifelse",
-                convert_expr(node.if_get_cond()),
-                build_ast(ctx, node.if_get_then()),
-                build_ast(ctx, node.if_get_else()))
-        else:
-            return (
-                "if",
-                convert_expr(node.if_get_cond()),
-                build_ast(ctx, node.if_get_then()))
-    elif node.get_type() == isl.ast_node_type.user:
-        return convert_stmt(ctx, node.user_get_expr())
+def transform_var(bmap, iterator_map, build):
+    pma = isl.PwMultiAff.from_map(isl.Map.from_union_map(iterator_map.apply_range(bmap)))
+    expr = build.access_from_pw_multi_aff(pma)
+    n = expr.get_op_n_arg()
+    return ('element', expr.get_op_arg(0).get_id().get_name() , tuple(convert_expr(expr.get_op_arg(i)) for i in xrange(1,n)))
+
+
+def transform_expr(expr, iterator_map, build):
+    if expr[0] == 'var':
+        return transform_var(expr[1], iterator_map, build)
+    elif expr[0] == 'const':
+        return expr
+    elif expr[0] == 'call':
+        return ('call', expr[1], tuple(transform_expr(e, iterator_map, build) for e in expr[2]))
     else:
         raise NotImplementedError
+
+
+def transform_stmt(stmt, iterator_map, build):
+    name = stmt[0].get_tuple_name(isl.dim_type.in_)
+
+    return ("assign", transform_var(stmt[0], iterator_map, build),
+            transform_expr(stmt[1], iterator_map, build), name)
+
+
+def codegen(ctx, constraints):
+    schedule = constraints.compute_schedule()
+    stmts = {}
+
+    def at_each_domain(node, build):
+        if node.get_type() != isl.ast_node_type.user:
+            return node
+
+        expr = node.user_get_expr()
+        assert expr.get_type() == isl.ast_expr_type.op
+        assert expr.get_op_type() == isl.ast_op_type.call
+        expr = expr.get_op_arg(0)
+        assert expr.get_type() == isl.ast_expr_type.id
+
+        name = expr.get_id().get_name()
+        stmt = get_stmt(ctx, name)
+
+        iterator_map = build.get_schedule().reverse()
+
+        name = "S"+str(len(stmts))
+        stmts[name] = transform_stmt(stmt, iterator_map, build)
+
+        return isl.AstNode.alloc_user(isl.AstExpr.from_id(isl.Id.alloc(schedule.get_ctx(), name, None)))
+
+    build = isl.AstBuild.alloc(ctx.isl_context)
+    build, _ = build.set_at_each_domain(at_each_domain)
+    node = build.node_from_schedule(schedule)
+
+    return build_ast(stmts, node)
 
 
 def get_schedule_constraints(ctx):
@@ -156,29 +146,18 @@ def get_schedule_constraints(ctx):
         def_map.domain()
         .union(init_map.domain())
         .union(update_map.domain()))
+
     validity = (
         def_map.apply_range(use_map.reverse())
         .union(init_map.apply_range(update_map.reverse()))
         .union(update_map.apply_range(def_map.reverse())))
 
-    coincidence = validity.union(update_map.apply_range(update_map.reverse()))
+    proximity = validity.union(update_map.apply_range(update_map.reverse()))
 
     constraints = (
         isl.ScheduleConstraints.on_domain(domain)
         .set_validity(validity)
-        .set_coincidence(coincidence)
-        .set_proximity(validity)
+        .set_proximity(proximity)
     )
 
     return constraints
-
-
-def get_schedule_map(constraints):
-    schedule = constraints.compute_schedule()
-    return schedule.get_map().intersect_domain(schedule.get_domain())
-
-
-def codegen(ctx, constraints):
-    schedule_map = get_schedule_map(constraints)
-    node = isl.AstBuild.alloc(ctx.isl_context).ast_from_schedule(schedule_map)
-    return build_ast(ctx, node)
